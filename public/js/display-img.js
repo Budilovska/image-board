61b6ace2f7aea688d05bfd9906cd1bed6a700049
Alona Budilovska <boodilovskaya@gmail.com>
2019-07-18 18:19:57 +0200

    Bug fixes

--- public/js/display-img.js
+++ public/js/display-img.js
@@ -1,37 +1,40 @@
 (function() {
-    Vue.component('display-img', {
-        template: '#display-template',
+    Vue.component("display-img", {
+        template: "#display-template",
         data: function() {
             return {
-                url: '',
-                title: '',
-                description: '',
-                username: '',
-                date: '',
-                comment: '',
-                author: '',
+                url: "",
+                title: "",
+                description: "",
+                username: "",
+                date: "",
+                comment: "",
+                author: "",
                 comments: []
-            }
+            };
         },
         props: ["id"],
         mounted: function() {
-            console.log('mounted!!!');
+            console.log("mounted!!!");
             console.log("this.comments", this.comments);
 
             var that = this;
-            axios.get('/comment/' + that.id).then(function(resp) {
-                console.log("RESP:", resp);
-                that.url = resp.data[0][0].url;
-                that.title = resp.data[0][0].title;
-                that.description = resp.data[0][0].description;
-                that.username = resp.data[0][0].username;
-                that.date = resp.data[0][0].created_at;
-                that.comments = resp.data[1];
-                console.log("that.comments", that.comments);
-            }).catch(function(err) {
-                    console.log('err in GET /comment', err);
-            }); //closes /comment get
-        },      //closes mounted
+            axios
+                .get("/comment/" + that.id)
+                .then(function(resp) {
+                    console.log("RESP:", resp);
+                    that.url = resp.data[0][0].url;
+                    that.title = resp.data[0][0].title;
+                    that.description = resp.data[0][0].description;
+                    that.username = resp.data[0][0].username;
+                    that.date = resp.data[0][0].created_at;
+                    that.comments = resp.data[1];
+                    console.log("that.comments", that.comments);
+                })
+                .catch(function(err) {
+                    console.log("err in GET /comment", err);
+                }); //closes /comment get
+        }, //closes mounted
         methods: {
             clickedSubmitCom: function(e) {
                 e.preventDefault();
@@ -39,27 +42,24 @@
                 // console.log(this.id);
                 var that = this;
 
-                axios.post('/comment/' + this.id, {comment: this.comment, author: this.author}).then(function(resp) {
-                    console.log('response from POST /comment', resp.data);
-                    that.comments.unshift(resp.data[0]);
-                }).catch(function(err) {
-                     console.log('error in POST /comment', err);
-                })
+                axios
+                    .post("/comment/" + this.id, {
+                        comment: this.comment,
+                        author: this.author
+                    })
+                    .then(function(resp) {
+                        console.log("response from POST /comment", resp.data);
+                        that.comments.unshift(resp.data[0]);
+                    })
+                    .catch(function(err) {
+                        console.log("error in POST /comment", err);
+                    });
             }
-        },
+        }
         // watch: {
         //     id: function() {
         //adding here all from mounted
         //     }
         // }
-
-
-
-    }) //end of vue component
-
-    })(); //end of efii
-
-    // the componen will need mounted function : take id it's passed, makes ajax req to get info about the image
-    // all data about the image and all comments
-    //
-    // new query get image by id - we pass id and get all infos
+    }); //end of vue component
+})(); //end of efii
--- public/js/script.js
+++ public/js/script.js
@@ -1,110 +1,81 @@
 (function() {
+    new Vue({
+        el: ".main",
+        data: {
+            images: [],
+            showModal: false,
+            title: "",
 
-new Vue({
-    el: '.main',
-    data: {
-        images: [],
-        showModal: false,
-        title: '',
+            description: "",
 
-        description: '',
+            username: "",
 
-        username:  '',
+            file: null,
 
-        file: null,
-
-        id: ''
-
-    }, //closes data
-    mounted: function() {
-
-
-        console.log('this', this)  //properties of data are added to "this"
-        var self = this;
-
-        axios.get('/images').then(function(resp) {
-
-            self.images = resp.data.rows;
-            // console.log('self.images:', self.images);
-
-        }).catch(function(err) {
-            console.log('err in GET /cities', err);
-        }); //closes axios req
-    },      //closes mounted - don't forget a coma!!!!
-
-    methods: {
-
-//----------------------------
-
-        handleClick: function(e) {
-            e.preventDefault();
-            // console.log('this', this)  //properties of data are added to "this"
-
-            // FormData API is necessary for sending FILES:
-            var formData = new FormData();
-            formData.append('title', this.title);//value "this.title" is whatever user typed in input field
-            formData.append('username', this.username);
-            formData.append('description', this.description);
-            formData.append('file', this.file);
-
+            id: ""
+        }, //closes data
+        mounted: function() {
+            console.log("this", this); //properties of data are added to "this"
             var self = this;
 
-            axios.post('/upload', formData).then(function(resp) {
-                console.log('response from POST /upload', resp);
-                self.images.unshift(resp.data);
+            axios
+                .get("/images")
+                .then(function(resp) {
+                    self.images = resp.data.rows;
+                    // console.log('self.images:', self.images);
+                })
+                .catch(function(err) {
+                    console.log("err in GET /cities", err);
+                }); //closes axios req
+        }, //closes mounted - don't forget a coma!!!!
 
-            }).catch(function(err) {
-                console.log('error in POST /upload', err);
-            })
-        },  //closes handleClick
+        methods: {
+            //----------------------------
 
-//------------------this function runs when user selects an image:
-        handleChange: function(e) {
-            // console.log('Handlechange:', e.target.files[0]);
-            // e - gives us an object we'll store inside of in data
-            //now we need to add is as a property of data:
-            this.file = e.target.files[0];
+            handleClick: function(e) {
+                e.preventDefault();
+                // console.log('this', this)  //properties of data are added to "this"
 
-        },  //closes handleChange
+                // FormData API is necessary for sending FILES:
+                var formData = new FormData();
+                formData.append("title", this.title); //value "this.title" is whatever user typed in input field
+                formData.append("username", this.username);
+                formData.append("description", this.description);
+                formData.append("file", this.file);
 
-        clicked: function(e) {
+                var self = this;
 
-            this.showModal = true;
-            console.log("IMAGES URL", this.images[0].url);
+                axios
+                    .post("/upload", formData)
+                    .then(function(resp) {
+                        console.log("response from POST /upload", resp);
+                        self.images.unshift(resp.data);
+                    })
+                    .catch(function(err) {
+                        console.log("error in POST /upload", err);
+                    });
+            }, //closes handleClick
 
-            this.images.forEach(obj => {
-                if (e.path[0].currentSrc == obj.url) {
-                // console.log(obj.id);
-                 this.id = obj.id;
-                }
-            })
-        }
-//---------------
-    } //closes methods
+            //------------------this function runs when user selects an image:
+            handleChange: function(e) {
+                // console.log('Handlechange:', e.target.files[0]);
+                // e - gives us an object we'll store inside of in data
+                //now we need to add is as a property of data:
+                this.file = e.target.files[0];
+            }, //closes handleChange
 
-});  //closes new vue
+            clicked: function(e) {
+                this.showModal = true;
+                console.log("IMAGES URL", this.images[0].url);
 
-// vue life cycle:
-// created
-// mounted
-// destroyed
-
-// mounted: runs after HTML has loaded. It's a "lifecycle method".
-// In "mounted" we're we're going to make ajax requests to get data the user wants to
-// see at the initial moment the page is loaded. If u're in the situation in which u want to render
-// information the moment the page is rendered ..u'll probably want to fetchthat data in the "mounted" function.
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+                this.images.forEach(obj => {
+                    if (e.path[0].currentSrc == obj.url) {
+                        // console.log(obj.id);
+                        this.id = obj.id;
+                    }
+                });
+            }
+            //---------------
+        } //closes methods
+    }); //closes new vue
 })();
